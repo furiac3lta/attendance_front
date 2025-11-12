d4113679ed708441c7e338f594afa2bb2c8c7da2
furiac3lta <colummarcelo@gmail.com>
2025-11-12 18:58:49 -0300

    conecction with heroku

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  production: false,
-  API_URL: 'http://localhost:8080/api' // URL de tu backend local
+  production: true,
+  API_URL: 'https://assist.herokuapp.com'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
- 
-  API_URL: 'http://localhost:8080/api'
+  production: true,
+  API_URL: 'https://assist.herokuapp.com'
 };
