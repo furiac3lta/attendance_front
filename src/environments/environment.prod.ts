f07d891ce59cb892005c1a4d5affd2312c83b0ae
furiac3lta <colummarcelo@gmail.com>
2025-11-13 01:43:21 -0300

    working for netlify deploy by git 3

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  API_URL: 'https://assist-13e403b2978a.herokuapp.com/api'
+  API_URL: 'https://attendancebackend-production-c255.up.railway.app/api'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  production: true,
-  API_URL: 'https://assist-13e403b2978a.herokuapp.com/api'
+  production: false,
+  API_URL: 'https://attendancebackend-production-c255.up.railway.app/api'
 };
