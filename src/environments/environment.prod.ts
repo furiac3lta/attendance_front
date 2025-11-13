05a7ce5d531782166a9264b605f66d19f30cfbe8
furiac3lta <colummarcelo@gmail.com>
2025-11-13 09:58:48 -0300

    change backend to heroku 1

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
  //API_URL: 'https://attendancebackend-production-c255.up.railway.app/api'
-API_URL:'https://assist-13e403b2978a.herokuapp.com/'
+API_URL:'https://assist-13e403b2978a.herokuapp.com/api'
 
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
  // API_URL: 'https://attendancebackend-production-c255.up.railway.app/api'
- API_URL:'https://assist-13e403b2978a.herokuapp.com/'
+ API_URL:'https://assist-13e403b2978a.herokuapp.com/api'
 };
